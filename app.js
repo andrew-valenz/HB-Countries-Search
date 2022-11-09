122878d633dab9627bfebb4f704858866b474980
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-11-09 12:57:19 -0800

    completed slice C

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ async function findCountries(continent) {
     const response = await getCountries(continent);
     // Slice C: add continent argument to getCountries function call
     // console log the response object to see all of the nested information returned
+    console.log(response);
     // Slice A: set the countries state to the response.data
     countries = response.data;
     // Slice A: call displayCountries function;
@@ -41,6 +42,7 @@ searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(searchForm);
     // Slice C: Call findCountries with continent from formData
+    findCountries(formData.get('continent'));
 });
 
 /* Display Functions */
